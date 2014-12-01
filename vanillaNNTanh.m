7b384d2f7b43773c540a8c6978ddb7bec53e7017
Hilal Alsibai <xiphirx@gmail.com>
2014-11-30 17:48:54 -0800

    Move vanilla sigm neural net to separate file, create tanh vanilla neural net

---
+++ vanillaNNTanh.m
@@ -0,0 +1,32 @@
+% Add Deep learning toolbox code
+addpath(genpath('DeepLearnToolbox'));
+
+% Add common code
+addpath(genpath('common'));
+
+% Load the train data if needed
+if (exist("mergedData", "var") == 0)
+	loadTrainData;
+end
+
+% Create neural net targeted output
+yTrain = zeros(size(trainData, 1), numClasses);
+for i = 1:numTrainSamples
+	yTrain(i, classification(i)) = 1;
+endfor
+
+% Normalize
+[normFeatures, mu, sigma] = zscore(features);
+
+% Construct 3 layer ANN with 5 hidden nodes and 7 output nodes for all features
+neuralNet = nnsetup([size(features, 2) 5 numClasses]);
+neuralNet.activation_function = 'tanh_opt';
+neuralNet.learningRate = 0.5;
+opts.numepochs = 50;
+opts.batchsize = 105;
+opts.plot = 1;
+
+% Train
+[neuralNet, L] = nntrain(neuralNet, normFeatures, yTrain, opts);
+
+L
