7b4dec46b79ac4732185da3dcbab17e898bebb25
Aaun Abbas <aabbas@ucdavis.edu>
2014-12-03 14:12:05 -0800

    Outputs a sub.csv file for Kaggle submission
    Submit the file on kaggle to check the accuracy of the classifier. Need
    to add column names to the csv file manually

--- randomforests.R
+++ randomforests.R
@@ -4,18 +4,19 @@ randomforestclf <- function(){
   train <- read.csv("./train.csv", header = F)
   test <- read.csv("./test.csv", header = T)
   
-  train <- train[,-1]
-  test <- test[,-1]
+  names(train)[1:55] <- names(test)
+  names(train)[56] <- "Cover_Type"
   
-  names(train)[1:54] <- names(test)
-  names(train)[55] <- "Cover_Type"
-  
-  cols <- names(train)[1:54]
-  response <- names(train)[55]
+  cols <- names(train)[2:55]
+  response <- names(train)[56]
   
   clf <- randomForest(train[,cols], y = factor(train[,response]))
   
   plot(clf)
   
   ans <- predict(clf,test)
+  
+  #preparing the data for kaggle submission 
+  fa <- as.numeric(levels(ans)[ans])
+  write.csv(fa, file = "sub.csv", row.names = test[,1]) #Still need to open this file in excel and change column to Id and Cover_Type according to the instructions.
 }
