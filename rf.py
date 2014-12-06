2022f1f480963372667938a7efb46622624f393d
Raymond Lau <rlau@Raymonds-MacBook-Pro.local>
2014-12-05 18:46:32 -0800

    changed max features

--- rf.py
+++ rf.py
@@ -5,6 +5,10 @@ df = np.loadtxt("/Users/rlau/repos/forestcover/covtype.data", delimiter=',')
 train_x = df[1:522909,0:12]
 train_y = df[1:522909,54]
 
-clf = RandomForestClassifier(n_estimators=10, max_features = 12)
+clf = RandomForestClassifier(n_estimators=10, max_features = 3)
 clf.fit(train_x, train_y)
 
+test_x = df[522910:581012,0:12]
+test_y = df[522910:581012,54]
+
+print np.sum(test_y==clf.predict(test_x)) / float(len(test))
