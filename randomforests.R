23e56d690a493f9db992758d7a6b2ef39ffc918c
Aaun Abbas <aabbas@ucdavis.edu>
2014-12-07 00:05:51 -0800

    Added titles

--- randomforests.R
+++ randomforests.R
@@ -23,7 +23,6 @@ randomforest <- function(k,end = 40) {
   colnames(data)[1:12] <- c("Elevation","Aspect", 'Slope', 'Hori_Dist_Hydro','Vert_Dist_Hydro','Hori_Dist_Road','Hillshade9am','Hillshadenoon','Hillshade3pm','Hori_Dist_Fire', 'Wilderness_Area','Soil_Type')
   
   cols = names(data)[1:12]
-  #print(cols[c(-11)])
   response <- names(data)[13]
   #########################
   
@@ -56,13 +55,15 @@ randomforest <- function(k,end = 40) {
   
   imp <- round(importance(clf, type = 1), 2)
   
-  print(imp)
-  
-  varImpPlot(clf,type = 1)
+  varImpPlot(clf,type = 1, main="Variable importance plot")
   
   ord <- order(imp[,1], decreasing = FALSE)
   
-  impcols <- ord[-(1:3)] #changing this will affect how many variables to excluse from the second run
+  a = 1
+  
+  impcols <- ord[-(1:a)] #changing this will affect how many variables to excluse from the second run
+  
+  cat("Run 2 excluded ", a, " variable(s)")
   
   impclf <- randomForest(x = train[,impcols], 
                          y = factor(train[,response]),
@@ -74,8 +75,7 @@ randomforest <- function(k,end = 40) {
                          keep.forest = TRUE)
   
   testmat <- cbind(clf$test$err.rate[, "Test"], impclf$test$err.rate[, "Test"])
-  print(nrow(testmat))
-  matplot(1:clf$ntree, testmat, type = "l", xlab = "Trees", ylab = "Error", col = 1:2)
-  legend("topright", legend = c("clf", "impclf"), lty = c(1,1), lwd = c(2.5,2.5), col = 1:2)
+  matplot(1:clf$ntree, testmat, type = "l", xlab = "Trees", ylab = "Error", col = 1:2, main = "Effect of removing variables with the least MeanDecreaseAccuracy")
+  legend("topright", legend = c("Run 1", "Run 2"), lty = c(1,1), lwd = c(2.5,2.5), col = 1:2)
 
 }
