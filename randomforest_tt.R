7cd15096b6acf5d49abdfda31f3bfa691b5e1fff
Aaun Abbas <aabbas@ucdavis.edu>
2014-12-14 07:29:40 -0800

    Added ROC curves
    The curves are very weird, I guess its good that they have high AUC
    values but it feels wrong.

--- randomforest_tt.R
+++ randomforest_tt.R
@@ -1,10 +1,22 @@
+#Summary
+#arguments:
+# k     the number of folds the data has
+# end   the number of trees to use in the forest
+# auc   Set to TRUE (default FALSE) to calculate multiclass AUC using methods in Hand and Till (2001)
+# roc   Set to TRUE (default FALSE) to get ROC curves
+# add   Set to FALSE to get seperate curves for all one vs all ROC curves
+
 library(randomForest)
 library(pROC)
+library(ROCR)
 
 # Trees vs Error
-randomforesttt <- function(k = 10,end = 40, auc = FALSE) {
-  k = 10
-  end = 40
+randomforesttt <- function(k = 10,end = 40, auc = FALSE, roc = FALSE, add = TRUE) { 
+  #k = 10
+  #end = 40
+  #roc = TRUE
+  #auc = FALSE
+  #add = TRUE
   rawdata <- read.csv("./covtype.data", header = F)
   
   s = nrow(rawdata)
@@ -29,10 +41,10 @@ randomforesttt <- function(k = 10,end = 40, auc = FALSE) {
   ##sample data##
   foldSize = floor(s/k)
   
-  range = sample(1:s, 2*foldSize, replace = FALSE) #pick random samples from data
+  range = sample(1:s, foldSize, replace = FALSE) #pick random samples from data
   
-  train <- data[range,]
-  test <- data[-range,]
+  train <- data[-range,]
+  test <- data[range,]
   
   ##################
   
@@ -45,6 +57,8 @@ randomforesttt <- function(k = 10,end = 40, auc = FALSE) {
   err <- clf$err.rate
   
   predictions <- testans$predicted
+  
+  votes <- testans$votes
 
   testmat <- cbind(clf$err.rate[,"OOB"], clf$test$err.rate[,"Test"])
 
@@ -53,5 +67,46 @@ randomforesttt <- function(k = 10,end = 40, auc = FALSE) {
   
   if(auc){
     multiclass.roc(test[,response],as.numeric(levels(predictions)[predictions]))
+  }
+  
+  if(roc){
+    
+    titles <- c("Spruce", "Lodgepole", "Ponderosa", "Cottonwood", "Aspen", "Douglas", "Krummholz")
+    
+    if(add){
+      resp <- ifelse(test[,response] == 1, 1, 0)
+      predobj <- prediction(votes[,1],resp)
+      perfobj <- performance(predobj, measure = "tpr", x.measure = "fpr")
+      auc <- (performance(predobj, measure = "auc"))@y.values
+      plot(perfobj, col = 1, main = "ROC curves" )
+      print(sprintf("AUC of %s is: %f", titles[1], auc[1]))
+      for(i in 2:7){
+        resp <- ifelse(test[,response] == i, 1, 0)
+        #pred <- ifelse(predictions == i, 1, 0)
+        predobj <- prediction(votes[,i],resp)
+        perfobj <- performance(predobj, measure = "tpr", x.measure = "fpr")
+        auc <- c(auc,(performance(predobj, measure = "auc"))@y.values)
+        plot(perfobj, col = i, add = TRUE)
+        print(sprintf("AUC of %s is: %f", titles[i], auc[i]))
+      }
+      legend("topright", legend = titles, lty = c(1,1), lwd = c(2.5,2.5), col = 1:7)
+    }
+    else{
+      resp <- ifelse(test[,response] == 1, 1, 0)
+      predobj <- prediction(votes[,1],resp)
+      perfobj <- performance(predobj, measure = "tpr", x.measure = "fpr")
+      auc <- (performance(predobj, measure = "auc"))@y.values
+      plot(perfobj, main = sprintf("ROC curve for %s", titles[1]))
+      print(sprintf("AUC of %s is: %f", titles[1], auc[1]))
+      for(i in 2:7){
+        resp <- ifelse(test[,response] == i, 1, 0)
+        #pred <- ifelse(predictions == i, 1, 0)
+        predobj <- prediction(votes[,i],resp)
+        perfobj <- performance(predobj, measure = "tpr", x.measure = "fpr")
+        auc <- c(auc,(performance(predobj, measure = "auc"))@y.values)
+        plot(perfobj, main = sprintf("ROC curve for %s", titles[i]))
+        print(sprintf("AUC of %s is: %f", titles[i], auc[i]))
+      }
+    }
   }
 }
